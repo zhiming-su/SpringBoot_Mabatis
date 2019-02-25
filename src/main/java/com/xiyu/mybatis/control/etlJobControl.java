2760a791b4bb40eb2e7f09f4982c825724b98bdd
苏志明 <zhiming.su@alphainsight.ai>
2019-02-25 17:12:47 +0800

    up

--- src/main/java/com/xiyu/mybatis/control/etlJobControl.java
+++ src/main/java/com/xiyu/mybatis/control/etlJobControl.java
@@ -1,11 +1,14 @@
 package com.xiyu.mybatis.control;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xiyu.mybatis.bean.etlJob;
 import com.xiyu.mybatis.test.Interface1;
 
 @RestController
@@ -18,4 +21,12 @@ public class etlJobControl {
 	public  String select() {
 		return ifd.select_mytest();
 	}
+	 @RequestMapping(value = "/select1", method = RequestMethod.GET)
+		public  Object select1() {
+			return ifd.select_mytest1();
+		}
+	 @RequestMapping(value = "/select2", method = RequestMethod.GET)
+		public  List<etlJob> select2() {
+			return ifd.select_mytest2();
+		}
 }
--- src/main/java/com/xiyu/mybatis/test/Interface1.java
+++ src/main/java/com/xiyu/mybatis/test/Interface1.java
@@ -1,6 +1,13 @@
 package com.xiyu.mybatis.test;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.xiyu.mybatis.bean.etlJob;
+
 public interface Interface1 {
 
 	 String select_mytest();
+	 List<HashMap<String,String>> select_mytest1();
+	 List<etlJob> select_mytest2();
 }
